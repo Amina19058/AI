20864b537791140670ff83d84e995e4e9d33eea2
Amina19058 <aminarenatovna@mail.ru>
2021-03-01 20:56:02 +0300

    2 track

--- backtrack.pl
+++ backtrack.pl
@@ -1,25 +1,19 @@
 actor(1, 1).
-
-generate_home_position(X, Y) :-
-    random(2, 9, X),
-    random(2, 9, Y).
-
-home(4, 4).
-covid(2, 3).
-doctor(5, 1).
-mask(6, 3).
+home(5, 5).
+covid(3, 3).
+covid(1, 5).
+doctor(1, 3).
+mask(8, 8).
+:- dynamic minimal_path/1.
+minimal_path([]).
 
 is_covid(X, Y) :-
     covid(X, Y).
 
-is_doctor(X, Y) :-
+get_protected(X, Y) :-
     doctor(X, Y).
-
-is_mask(X, Y) :-
-    mask(X, Y).
-
-is_home(X, Y) :-
-    home(X, Y).
+get_protected(X, Y) :-
+     mask(X, Y).
 
 is_infected(X, Y) :-
       covid(X1, Y1),
@@ -38,97 +32,115 @@ move_up_left(X, Y, New_X, New_Y) :- New_X is X - 1, New_Y is Y + 1.
 move_down_left(X, Y, New_X, New_Y) :- New_X is X - 1, New_Y is Y - 1.
 
 valid_move(X, Y) :- 
- between(1, 4, X),
- between(1, 4, Y).
+ between(1, 5, X),
+ between(1, 5, Y).
 
 i_am_at(X, Y) :-
     write('('), write(X), write(','), write(Y), writeln(')').
-   
+
 recursive_walk(X, Y, Visited, Protected) :-
        (   
-       home(X, Y) ->  write("Lovely home: "), i_am_at(X, Y), 
-       write("my path:"), writeln(Visited), false
+        home(X, Y) ->  %write("Lovely home: "), length(Visited, Len), write(Len),
+        %write("my path:"), writeln(Visited), 
+        minimal_path(Z),
+        (
+            length(Z, 0) -> retract(minimal_path(Z)),
+            asserta(minimal_path(Visited))
+            ;
+            length(Z, L1), length(Visited, L2),
+            ( 
+            L1 > L2 ->
+            retract(minimal_path(Z)),
+            asserta(minimal_path(Visited))
+            )
+        ),
+        false
        );
-      
       (   
-    
         (
         move_up(X, Y, New_X, New_Y), valid_move(New_X, New_Y), 
         not(member((New_X;New_Y), Visited)), 
-        not(is_infected(New_X, New_Y)) ->  
+        (Protected; not(is_infected(New_X, New_Y))) ->  
         %i_am_at(New_X, New_Y), 
         append(Visited, [(New_X;New_Y)], Visited1), 
-        recursive_walk(New_X, New_Y, Visited1) 
+        recursive_walk(New_X, New_Y, Visited1, (Protected ; get_protected(New_X, New_Y))) 
         ; false
         );
 
         (   
         move_up_right(X, Y, New_X, New_Y), valid_move(New_X, New_Y),
         not(member((New_X;New_Y), Visited)), 
-        not(is_infected(New_X, New_Y)) ->  
+        (Protected; not(is_infected(New_X, New_Y))) ->  
         %i_am_at(New_X, New_Y), 
         append(Visited, [(New_X;New_Y)], Visited1), 
-        recursive_walk(New_X, New_Y, Visited1)
+        recursive_walk(New_X, New_Y, Visited1, (Protected; get_protected(New_X, New_Y)))
         ; false
         );
 
         (   
         move_up_left(X, Y, New_X, New_Y), valid_move(New_X, New_Y),
         not(member((New_X;New_Y), Visited)), 
-        not(is_infected(New_X, New_Y)) ->  
+        (Protected; not(is_infected(New_X, New_Y))) ->
         %i_am_at(New_X, New_Y), 
         append(Visited, [(New_X;New_Y)], Visited1), 
-        recursive_walk(New_X, New_Y, Visited1)
+        recursive_walk(New_X, New_Y, Visited1, (Protected; get_protected(New_X, New_Y)))
         ; false
         );
 
         (   
         move_down(X, Y, New_X, New_Y), valid_move(New_X, New_Y),
         not(member((New_X;New_Y), Visited)), 
-        not(is_infected(New_X, New_Y)) ->  
+        (Protected; not(is_infected(New_X, New_Y))) ->  
         %i_am_at(New_X, New_Y), 
         append(Visited, [(New_X;New_Y)], Visited1), 
-        recursive_walk(New_X, New_Y, Visited1)
+        recursive_walk(New_X, New_Y, Visited1, (Protected; get_protected(New_X, New_Y)))
         ; false
         );
 
         (   
         move_down_right(X, Y, New_X, New_Y), valid_move(New_X, New_Y),
         not(member((New_X;New_Y), Visited)), 
-        not(is_infected(New_X, New_Y)) ->  
+        (Protected; not(is_infected(New_X, New_Y))) -> 
         %i_am_at(New_X, New_Y), 
         append(Visited, [(New_X;New_Y)], Visited1), 
-        recursive_walk(New_X, New_Y, Visited1)
+        recursive_walk(New_X, New_Y, Visited1, (Protected; get_protected(New_X, New_Y)))
         ; false
         );
 
         (   
         move_down_left(X, Y, New_X, New_Y), valid_move(New_X, New_Y),
         not(member((New_X;New_Y), Visited)),
-        not(is_infected(New_X, New_Y)) -> 
+        (Protected; not(is_infected(New_X, New_Y))) ->
         %i_am_at(New_X, New_Y), 
         append(Visited, [(New_X;New_Y)], Visited1), 
-        recursive_walk(New_X, New_Y, Visited1)
+        recursive_walk(New_X, New_Y, Visited1, (Protected; get_protected(New_X, New_Y)))
         ; false
         );
 
         (   
         move_right(X, Y, New_X, New_Y), valid_move(New_X, New_Y),
         not(member((New_X;New_Y), Visited)), 
-        not(is_infected(New_X, New_Y)) ->  
+        (Protected; not(is_infected(New_X, New_Y))) -> 
         %i_am_at(New_X, New_Y), 
         append(Visited, [(New_X;New_Y)], Visited1), 
-        recursive_walk(New_X, New_Y, Visited1)
+        recursive_walk(New_X, New_Y, Visited1, (Protected; get_protected(New_X, New_Y)))
         ; false
         );
 
         (   
         move_left(X, Y, New_X, New_Y), valid_move(New_X, New_Y),
         not(member((New_X;New_Y), Visited)), 
-        not(is_infected(New_X, New_Y)) ->  
+        (Protected; not(is_infected(New_X, New_Y))) -> 
         %i_am_at(New_X, New_Y), 
         append(Visited, [(New_X;New_Y)], Visited1), 
-        recursive_walk(New_X, New_Y, Visited1)
+        recursive_walk(New_X, New_Y, Visited1, (Protected; get_protected(New_X, New_Y)))
         ; false
         )
       ).
+game() :-
+    recursive_walk(1, 1, [(1;1)], get_protected(1, 1));
+    (minimal_path(Z), write(Z)).
+
+ main :- write('').
+ ?- game().
+
